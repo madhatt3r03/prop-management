c85c0a66c4339c62449dbf420e9ae8a973ab04ae
John Phillips <madhatter003@yahoo.com>
2019-03-04 12:33:24 -0700

    tabnav grid

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -25,7 +25,12 @@ class Dashboard extends Component {
     }
 
     render() {
-      return <TabNav tabs={this.state.tabs}/>
+      return (
+        <div className='dashboard'>
+            <TabNav tabs={this.state.tabs}/>
+        </div>
+      )
+      
     }
 
   }
--- src/components/tabnav.js
+++ src/components/tabnav.js
@@ -3,16 +3,12 @@ import React, { Component } from 'react';
 
 class TabNav extends Component {
     render() {
+        return (
+            <div className='tab-nav'>
 
-     var JSX = [
-         <h1>TABS</h1>
-     ];
-        
-     this.props.tabs.map((tab, index) => {
-        JSX.push(tab.component);
-     })   
-        
-    return JSX;
+            </div>
+        )
+
     }
   }
 
