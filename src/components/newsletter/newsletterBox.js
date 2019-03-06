8fa6ca7ba7add26d32f82e2ab610a378d43ecc68
John Phillips <madhatter003@yahoo.com>
2019-03-06 15:31:47 -0700

    styled the newsletter box component

--- src/components/newsletter/newsletterBox.js
+++ src/components/newsletter/newsletterBox.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sept','Oct','Nov','Dec',]
 
-export default function NewsletterBox(date) {
+export default function NewsletterBox({date}) {
     return (
         <div className='newsletter-box'>
-            <div className='newsletter-box_day'>{date.getDate()}</div>
+            <div className='newsletter-box__day'>{date.getDate()}</div>
             <div className='newsletter-box__month-year'>{months[date.getMonth()]} {date.getFullYear()}</div>
         </div>
     )
